815c6cdde7132ff65d046e76bacb2bdb7198cdb9
Leonard <liyangde66@gmail.com>
2015-05-08 17:33:45 +0800

    add Treasurebox not finished

--- Classes/TreasureBox.h
+++ Classes/TreasureBox.h
@@ -1,5 +1,19 @@
 #include "cocos2d.h"
+#include "ui/CocosGUI.h"
+enum class ScenarioEnum;
+class Equipment;
 
-class TreasureBox :public cocos2d::Node{
+class TreasureBox :public cocos2d::ui::Button{
+public:
+	TreasureBox();
+	~TreasureBox();
+	static TreasureBox* create(ScenarioEnum);
 
+
+private:
+	void __loadCSB();
+	void __createEquipment(ScenarioEnum);
+	virtual bool init(ScenarioEnum);
+	
+	Equipment* m_equ;
 };
--- Classes/models/TreasureBox.cpp
+++ Classes/models/TreasureBox.cpp
@@ -1,2 +1,59 @@
 #include "TreasureBox.h"
+#include "GameScene.h"
+#include "JsonUtility.h"
+#include "cocos2d.h"
+USING_NS_CC;
+TreasureBox::TreasureBox(){
+}
 
+TreasureBox::~TreasureBox(){
+
+}
+
+TreasureBox* TreasureBox::create(ScenarioEnum scenario){
+	TreasureBox* treasure = new (std::nothrow)TreasureBox;
+	if (treasure && treasure->init(scenario))
+	{
+		treasure->autorelease();
+		return treasure;
+	}
+	CC_SAFE_DELETE(treasure);
+	return treasure;
+}
+
+bool TreasureBox::init(ScenarioEnum scenario){
+	if (!Node::init())
+	{
+		return false;
+	}
+
+
+	__loadCSB();
+
+	return true;
+}
+
+void TreasureBox::__loadCSB(){
+	
+}
+
+void TreasureBox::__createEquipment(ScenarioEnum scenario){
+	 m_equ = Equipment::create("Default/Button_Disable.png","Default/Button_Disable.png","Default/Button_Disable.png");
+	int i = static_cast<int>(scenario);	
+	int j =	RandomHelper::random_int(0, 3);	
+	int ID = j * 3 + i;
+
+	EquipmentInfo info = JsonUtility::getInstance()->getEquipment(ID);
+	float percent = RandomHelper::random_real(0.8f, 1.2f);
+	m_equ->setAttack(info.Attack * percent);
+	percent = RandomHelper::random_real(0.8f, 1.2f);
+	m_equ->setAttackRate(info.AttackRate * percent);
+	percent = RandomHelper::random_real(0.8f, 1.2f);
+	m_equ->setDenfense(info.Defense * percent);
+	percent = RandomHelper::random_real(0.8f, 1.2f);
+	m_equ->setMoveRate(info.MoveRate * percent);
+	percent = RandomHelper::random_real(0.8f, 1.2f);
+	m_equ->setIntelligence(info.Intelligence * percent);
+	percent = RandomHelper::random_real(0.8f, 1.2f);
+	m_equ->setBlood(info.Blood * percent);
+}
